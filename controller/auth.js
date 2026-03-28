274cfa509b9790744fd43847112221e419733deb
Tamanna_verma <tamannaverma7389@gmail.com>
2026-03-28 11:40:36 +0530

    Initial commit

--- controller/auth.js
+++ controller/auth.js
@@ -4,9 +4,11 @@ import bcrypt from "bcrypt";
 export async function register(req, res) {
   try {
     const { username, email, password } = req.body;
-
     if (!username || !email || !password) {
-      throw new Error("All fields are required");
+      return res.status(400).json({
+        success: false,
+        message: "All fields are required"
+      });
     }
     const normalizedEmail = email.toLowerCase();
     const hashedPassword = await bcrypt.hash(password, 10);
@@ -22,20 +24,16 @@ export async function register(req, res) {
     expiresIn: "15m"
     }
 );
-
     res.cookie("accessToken", accessToken , {
     httpOnly:true,
         secure: false,
         sameSite: "strict",
         maxAge: 7 * 24 * 60 * 60 * 1000
 });
-    
     res.status(201).json({
       success: true,
       message: "User registered successfully"
     });
-    
-
   } catch (error) {
     res.status(400).json({
       success: false,
@@ -49,7 +47,10 @@ export async function login(req, res) {
     const {  email, password } = req.body;
 
     if (!email || !password) {
-      throw new Error("All fields are required");
+      return res.status(400).json({
+        success: false,
+        message: "All fields are required"
+      });
     }
    const normalizedEmail = email.toLowerCase();
     const user = await usermodel.findOne({ email: normalizedEmail });
@@ -59,7 +60,7 @@ export async function login(req, res) {
     const isMatch = await bcrypt.compare(password, user.password);
  
     if (!isMatch) {
-     throw new Error("Invalid password");
+     throw new Error("Invalid email or password");
     } 
     
     let accessToken = jwt.sign({
